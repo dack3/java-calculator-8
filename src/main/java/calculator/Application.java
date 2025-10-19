ac7d7fba1a26cecdcacb1b683dd88f81d305fdef
dack3 <joyunjeong75@gmail.com>
2025-10-20 01:54:53 +0900

    [feat] 커스텀 구분자가 없는 경우의 문자열을 숫자로 변환

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,21 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String input = Console.readLine();
+        int num = parNum(input);
     }
+    public static int parNum(String input){
+        //contains는 booelan 반환
+        if(!input.contains("/")){
+            input.replaceAll("[;.]","");
+            int num = Integer.valueOf(input);
+            return num;
+        } else{
+            return 0;
+        }
+    }
+
 }
